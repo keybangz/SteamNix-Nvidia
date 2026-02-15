4cf62391d54ceef0a5a2f0df31c5ce23ff518111
keybangz <wyatt.wheeler@protonmail.com>
2026-02-15 22:25:41 +1300

    Remove unused jupiter archive fix

--- jupiter_archive_fix.sh
+++
@@ -1,3 +0,0 @@
-while nix-build --no-out-link --arg nixpkgs 'import <nixpkgs> {}' --attr pkgs.jupiter-hw-support.src.src; do
-  nix-store --delete $( nix-instantiate --eval --expr '"${(import ./. {}).pkgs.jupiter-hw-support.src.src}"' | tr -d '"' )
-done
